c39a63fb3414501d564c69b8b30e3c62f6eb9e12
Sergio Bershadsky <sergio.bershadsky@gmail.com>
2020-10-07 12:09:08 +0300

    REMOVE: SyncClient

--- hummingbot/connector/exchange/idex/utils.py
+++ hummingbot/connector/exchange/idex/utils.py
@@ -1,18 +1,47 @@
+import functools
 import re
+import time
 
 from typing import Optional
 
-from .client.sync import SyncIdexClient
+import typing
+
+from .client.asyncio import AsyncIdexClient
+from .types.rest.response import RestResponseAsset
 
 
-ASSETS = [asset.symbol for asset in SyncIdexClient().public.get_assets()]
-TRADING_PAIR_SPLITTER = re.compile(rf"^(\w+)({'|'.join(ASSETS)})$")
+def no_arg_cache(f=None, *, for_seconds=30):
+    def decorator(f):
+        @functools.wraps(f)
+        async def wrapper():
+            now = time.time()
+            result, created = getattr(f, "_result", (None, None))
+            if not result or (created + for_seconds) < now:
+                result = await f()
+                setattr(f, "_result", (result, now))
+            return result
+        return wrapper
+    if f:
+        return decorator(f)
+    return decorator
 
 
-def to_idex_pair(pair: str) -> Optional[str]:
+@no_arg_cache
+async def get_trading_pairs() -> typing.List[str]:
+    return [asset.symbol for asset in (await AsyncIdexClient().public.get_assets())]
+
+
+@no_arg_cache
+async def get_trading_pair_splitter() -> typing.Pattern:
+    pairs = await get_trading_pairs()
+    return re.compile(rf"^(\w+)({'|'.join(pairs)})$")
+
+
+async def to_idex_pair(pair: str) -> Optional[str]:
     try:
-        m = TRADING_PAIR_SPLITTER.match(pair)
-        return f"{m.group(1)}-{m.group(2)}"
+        pattern = await get_trading_pair_splitter()
+        matcher = pattern.match(pair)
+        return f"{matcher.group(1)}-{matcher.group(2)}"
     # TODO: What the ... is that below?
     # Exceptions are now logged as warnings in trading pair fetcher
     except Exception:
