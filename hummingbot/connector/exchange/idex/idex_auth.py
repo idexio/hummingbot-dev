21a43681bb6ffd7d76c9d5d834d2fa739bc48645
Sergio Bershadsky <sergio.bershadsky@gmail.com>
2020-11-06 18:04:17 +0300

    FIX: hex_to_uint128 ADD: get_wallet method ADD: hex_to_uint128 tests ADD: wallet signature ADD: wallet signature tests ADD: wallet signature propagation at rest FIX: url propagation at POSTs

--- hummingbot/connector/exchange/idex/idex_auth.py
+++ hummingbot/connector/exchange/idex/idex_auth.py
@@ -4,15 +4,15 @@ import string
 import uuid
 import hashlib
 
-from typing import Dict, Union
+from typing import Dict, Union, Tuple, Any
 from urllib.parse import urlencode
 
 from eth_account import Account
 from eth_account.messages import encode_defunct
+from eth_account.signers.local import LocalAccount
 from web3 import Web3
 
 from hummingbot.connector.exchange.idex.conf import settings
-from hummingbot.core.event.events import OrderType, TradeType
 
 
 class IdexAuth:
@@ -31,66 +31,47 @@ class IdexAuth:
     HEX_DIGITS_SET = set(string.hexdigits)
 
     @classmethod
-    def arrayif(cls, value: str):
-        # Remove 0x
-        value = value.rsplit("x", 1)[-1]
-        # Filter none hex
-        value = ''.join([c for c in value if c in cls.HEX_DIGITS_SET])
-        # Split by 2
-        value = [value[i:i + 2] for i in range(0, len(value), 2)]
-        # Convert to array
-        return list(map(lambda v: int(v, 16), value))
-
-    @classmethod
     def hex_to_uint128(cls, value):
-        # Remove 0x
-        value = value.rsplit("x", 1)[-1]
+        # Deal with leading zero
+        if value.startswith("0x"):
+            value = value[2:]
         # Filter none hex
-        value = ''.join([c for c in value if c in cls.HEX_DIGITS_SET])
+        value = '0x' + ''.join([c for c in value if c in cls.HEX_DIGITS_SET])
         return int(value, 16)
 
-    def sign_wallet(self,
-                    nonce: str,
-                    wallet: str,
-                    market: str,
-                    order_type: OrderType,
-                    order_side: TradeType,
-                    order_quantity: str,
-                    order_price: str = None,
-                    order_stop_price: str = None,
-                    order_custom_client_order_id: str = None,
-                    order_time_in_force: int = None,
-                    order_self_trade_trevention: int = None,
-                    private_key: str = None):
-
+    @classmethod
+    def get_wallet(cls, private_key: str = None) -> LocalAccount:
         private_key = private_key or settings.eth_account_private_key
+        return Account.from_key(private_key)
 
-        parameters = [
-            ["uint8", 1],
-            ["uint128", self.hex_to_uint128(nonce)],
-            ["address", wallet],
-            ["string", market],
-            ["uint8", order_type],
-            ["uint8", order_side],
-            ["string", order_quantity],
-            ["bool", bool(order_quantity)],
-            ["string", order_price or ""],
-            ["string", order_stop_price or ""],
-            ["string", order_custom_client_order_id or ""],
-            ["uint8", order_time_in_force],
-            ["uint8", order_self_trade_trevention or 0],
-            ["uint64", 0]
-        ]
+    @classmethod
+    def wallet_signature(cls, *parameters: Tuple[str, Any], private_key: str = None):
+        # parameters = [
+        #     ["uint8", 1],
+        #     ["uint128", self.hex_to_uint128(nonce)],
+        #     ["address", wallet.address],
+        #     ["string", market],
+        #     ["uint8", order_type],
+        #     ["uint8", order_side],
+        #     ["string", order_quantity],
+        #     ["bool", bool(order_quantity)],
+        #     ["string", order_price or ""],
+        #     ["string", order_stop_price or ""],
+        #     ["string", order_custom_client_order_id or ""],
+        #     ["uint8", order_time_in_force],
+        #     ["uint8", order_self_trade_trevention or 0],
+        #     ["uint64", 0]
+        # ]
 
         fields = [item[0] for item in parameters]
         values = [item[1] for item in parameters]
 
         signature_parameters_hash = Web3.solidityKeccak(fields, values)
-
-        return Account.sign_message(
-            signable_message=encode_defunct(text=signature_parameters_hash.hex()),
-            private_key=private_key
+        signed_message = cls.get_wallet(private_key=private_key).sign_message(
+            signable_message=encode_defunct(hexstr=signature_parameters_hash.hex())
         )
+
+        return signed_message.signature.hex()
 
     @staticmethod
     def generate_nonce():
@@ -101,17 +82,19 @@ class IdexAuth:
             http_method: str,
             url: str,
             params: Dict[str, any] = None,
-            body: Dict[str, any] = None) -> Dict[str, any]:
+            body: Dict[str, any] = None,
+            wallet_signature: str = None) -> Dict[str, any]:
         http_method = http_method.strip().lower()
         params = params or {}
         body = body or {}
-        return getattr(self, f"generate_auth_dict_for_{http_method}")(url, params, body)
+        return getattr(self, f"generate_auth_dict_for_{http_method}")(url, params, body, wallet_signature)
 
     def generate_auth_dict_for_get(
             self,
             url: str,
             params: Dict[str, any],
-            body: Dict[str, any] = None) -> Dict[str, any]:
+            body: Dict[str, any] = None,
+            wallet_signature: str = None) -> Dict[str, any]:
 
         if "nonce" not in params:
             params.update({
@@ -132,7 +115,8 @@ class IdexAuth:
             self,
             url: str,
             params: Dict[str, any],
-            body: Dict[str, any]) -> Dict[str, any]:
+            body: Dict[str, any],
+            wallet_signature: str = None) -> Dict[str, any]:
         body = body or {}
         parameters = body.get("parameters")
         if isinstance(parameters, dict) and "nonce" not in parameters:
@@ -140,7 +124,10 @@ class IdexAuth:
                 "nonce": self.generate_nonce()
             })
 
-        body = json.dumps(body)
+        if wallet_signature:
+            body["signature"] = wallet_signature
+
+        body = json.dumps(body, separators=(',', ':'))
 
         return {
             "headers": {
@@ -148,6 +135,7 @@ class IdexAuth:
                 "IDEX-HMAC-Signature": self.sign(body)
             },
             "body": body,
+            "url": url
         }
 
     generate_auth_dict_for_delete = generate_auth_dict_for_post
