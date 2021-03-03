600fdd058174327f98934d6d69c1a87141a1e5ce
bmorenus <58372262+bmorenus@users.noreply.github.com>
2021-03-03 18:14:53 -0500

    updated idex_rest_url and fetch_trading_pairs testing for api_order_book_data_source

--- test/integration/test_idex_api_order_book_data_source.py
+++ test/integration/test_idex_api_order_book_data_source.py
@@ -21,12 +21,21 @@ class TestDataSource (unittest.TestCase):
     def setUpClass(cls) -> None:
         cls.ev_loop: asyncio.BaseEventLoop = asyncio.get_event_loop()
         cls.order_book_data_source: IdexAPIOrderBookDataSource = IdexAPIOrderBookDataSource(cls.trading_pairs)
-        cls.idex_rest_url: str = IdexAPIOrderBookDataSource.get_idex_rest_url()
-        print(cls.idex_rest_url)
 
     def run_async(self, task):
         return self.ev_loop.run_until_complete(task)
 
+    # Test returns:
+    # Failure
+    # >>  raise self.failureException("'ETH' != 'https://api-None.idex.io'\n- ETH\n+ https://api-None.idex.io\n")
+    # get_idex_rest_url() returns url with blockchain = None when called.
+    def test_get_idex_rest_url(self):
+        self.assertEqual("ETH", IdexAPIOrderBookDataSource.get_idex_rest_url())
+
+    # Test returns:
+    # Failure
+    # >> raise self.failureException("'UNI-ETH' not found in []")
+    # test returns empty trading_pair list
     def test_fetch_trading_pairs(self):
         trading_pairs: List[str] = self.run_async(self.order_book_data_source.fetch_trading_pairs())
         self.assertIn("UNI-ETH", trading_pairs)
