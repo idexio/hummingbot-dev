97d3d43d2fbd3ec764d3211db2c14a4fccd3f79e
bmorenus <58372262+bmorenus@users.noreply.github.com>
2021-03-09 13:33:44 -0500

    updated timestamp conversion in listen_for_trades and listen_for_orderbook_diffs

--- hummingbot/connector/exchange/idex/idex_api_order_book_data_source.py
+++ hummingbot/connector/exchange/idex/idex_api_order_book_data_source.py
@@ -275,8 +275,8 @@ class IdexAPIOrderBookDataSource(OrderBookTrackerDataSource):
                         elif msg_type == "error":
                             raise ValueError(f"Idex Websocket received error message - {msg['data']['message']}")
                         elif msg_type == "trades":
-                            diff_timestamp: float = msg["data"]["t"]
-                            trade_msg: OrderBookMessage = IdexOrderBook.trade_message_from_exchange(msg, diff_timestamp)
+                            trade_timestamp: float = pd.Timestamp(msg["data"]["t"], unit='ms', tz='utc').timestamp()
+                            trade_msg: OrderBookMessage = IdexOrderBook.trade_message_from_exchange(msg, trade_timestamp)
                             output.put_nowait(trade_msg)
                         else:
                             raise ValueError(f"Unrecognized Idex WebSocket message received - {msg}")
@@ -330,7 +330,7 @@ class IdexAPIOrderBookDataSource(OrderBookTrackerDataSource):
                             raise ValueError(f"Idex WebSocket message received error message - "
                                              f"{msg['data']['message']}")
                         elif msg_type == "l2orderbook":
-                            diff_timestamp: float = msg["data"]["t"]
+                            diff_timestamp: float = pd.Timestamp(msg["data"]["t"], unit='ms', tz='utc').timestamp()
                             order_book_message: OrderBookMessage = \
                                 IdexOrderBook.diff_message_from_exchange(msg, diff_timestamp)
                             output.put_nowait(order_book_message)
