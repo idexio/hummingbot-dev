ad0665013cdca188d4780a61138bd8196286c73f
bmorenus <58372262+bmorenus@users.noreply.github.com>
2021-03-11 17:34:54 -0500

    removed volume_for_ask/bid_price. Coinbase used these functions due to the complexity of their ws DIFF feed, but they are not required for the idex feed

--- hummingbot/connector/exchange/idex/idex_active_order_tracker.pyx
+++ hummingbot/connector/exchange/idex/idex_active_order_tracker.pyx
@@ -45,24 +45,6 @@ cdef class IdexActiveOrderTracker:
         """
         return self._active_bids
 
-    def volume_for_ask_price(self, price) -> float:
-        """
-        For a certain price, get the volume sum of all ask order book rows with that price
-        :returns: volume sum
-        """
-
-        # todo: Confirm "remaining_size as property name"
-        return sum([float(msg["remaining_size"]) for msg in self._active_asks[price].values()])
-
-    def volume_for_bid_price(self, price) -> float:
-        """
-        For a certain price, get the volume sum of all bid order book rows with that price
-        :returns: volume sum
-        """
-
-        # todo: Confirm "remaining_size as property name"
-        return sum([float(msg["remaining_size"]) for msg in self._active_bids[price].values()])
-
     def get_rates_and_quantities(self, entry) -> tuple:
         # price, size
         return float(entry[0]), float(entry[1])
