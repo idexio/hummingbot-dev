383cbb2ca04ca79de9cf3744c6398cd22077a31e
bmorenus <58372262+bmorenus@users.noreply.github.com>
2021-03-24 11:58:26 -0400

    updated testing

--- test/integration/test_idex_market3.py
+++ test/integration/test_idex_market3.py
@@ -195,7 +195,7 @@ class IdexExchangeUnitTest(unittest.TestCase):
         self.assertEqual(len(limit_fee.flat_fees), 0)
         market_fee: TradeFee = self.market.get_fee("DIL", "ETH", OrderType.LIMIT, TradeType.BUY, 1)
         self.assertGreater(market_fee.percent, 0)
-        self.assertEqual(len(limit_fee.flat_fees), 1)
+        self.assertEqual(len(market_fee.flat_fees), 1)
 
     def test_fee_overrides_config(self):
         fee_overrides_config_map["idex_taker_fee"].value = None
