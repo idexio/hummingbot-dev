ba43f20c84427559efebbb6b51e5c803b93cb9b3
bmorenus <58372262+bmorenus@users.noreply.github.com>
2021-03-25 23:02:01 -0400

    adjusted spacing

--- test/integration/test_idex_market3.py
+++ test/integration/test_idex_market3.py
@@ -63,11 +63,9 @@ WS_BASE_URL = "wss://websocket-eth.idex.io/v1/"
 
 # load config from Hummingbot's central debug conf
 # Values can be overridden by env variables (in uppercase). Example: export IDEX_WALLET_PRIVATE_KEY="1234567"
-
 IDEX_API_KEY = getattr(conf, 'idex_api_key')
 IDEX_API_SECRET_KEY = getattr(conf, 'idex_api_secret_key')
 IDEX_WALLET_PRIVATE_KEY = getattr(conf, 'idex_wallet_private_key')
-
 
 # force resolution of api base url for conf values provided to this test
 hummingbot.connector.exchange.idex.idex_resolve._IS_IDEX_SANDBOX = True
